66083ef116fdbed744d930ba73c44c4da68151f6
furkanimir <frk.xw7@gmail.com>
2024-03-04 22:58:04 +0300

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import { authOptions } from "@/app/utils/deneme";
 import NextAuth from "next-auth";
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST, authOptions };
 
